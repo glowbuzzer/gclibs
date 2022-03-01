9b2115733614c72b5d0d18c4e5505bfc0abea683
David <david@glowbuzzer.com>
2022-03-01 13:45:27 +0000

    threec

---
+++ unity/inc/unity_config.h
@@ -0,0 +1,24 @@
+/**
+ ******************************************************************************
+ * @file           :  unity_config.h
+ * @brief          :
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2022 Glowbuzzer.
+ * All rights reserved.</center></h2>
+ *
+ ******************************************************************************
+ */
+
+
+#ifndef GBEM_UNITY_CONFIG_H
+#define GBEM_UNITY_CONFIG_H
+
+
+#define UNITY_OUTPUT_COLOR
+#define UNITY_INCLUDE_EXEC_TIME
+#define UNITY_INCLUDE_PRINT_FORMATTED
+#define UNITY_INCLUDE_DOUBLE
+
+#endif //GBEM_UNITY_CONFIG_H
