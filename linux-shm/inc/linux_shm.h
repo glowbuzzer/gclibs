169e452cb59d43b3d14a9a8ff871eca2406f89e5
David <david@glowbuzzer.com>
2023-08-26 15:07:30 +0100

    chore: Added AW-J17 drive code

--- linux-shm/inc/linux_shm.h
+++ linux-shm/inc/linux_shm.h
@@ -14,6 +14,7 @@
 
 #ifndef GCLIB__LINUX_SHM_H
 #define GCLIB__LINUX_SHM_H
+
 #include "stdint.h"
 #include "gberror.h"
 #include <stdbool.h>
@@ -35,9 +36,10 @@ struct shm_msg {
     uint8_t sm_buf_in[SHM_BUF_SIZE];
     uint8_t sm_buf_out[SHM_BUF_SIZE];
 };
-//extern struct shm_msg *shmp;
 
-gberror_t establish_shared_mem_and_signal_con(struct shm_msg **shared_mem, char *proc, const bool retry, int *pid, int um_en);
+
+gberror_t establish_shared_mem_and_signal_con(struct shm_msg **shared_mem, char *proc, bool retry, int *pid, int um_en);
+
 gberror_t establish_shared_mem_con(struct shm_msg **shared_mem, int um_en);
 
 
