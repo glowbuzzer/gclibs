e4a8baaff60eed5a7ea7f1e62ad49c36ab2a949a
David <david@glowbuzzer.com>
2023-09-03 09:01:02 +0100

    chore: Drive tidy up and dpm offline basis

--- dpm/inc/dpm.h
+++ dpm/inc/dpm.h
@@ -32,14 +32,14 @@ typedef struct {
     uint32_t machine_word; //the machine's overall status (CiA-402)
     uint32_t active_fault_word; //bits that define any active faults with the machine
     uint32_t fault_history_word; //bits that define any fault events that resulted in an error
-    uint32_t  heartbeat; // a periodically increasing heartbeat used to detect we are connected
-    uint16_t  joint_statusword[DPM_NUM_JOINTS]; //the CiA-402 status for drives
+    uint32_t heartbeat; // a periodically increasing heartbeat used to detect we are connected
+    uint16_t joint_statusword[DPM_NUM_JOINTS]; //the CiA-402 status for drives
     int32_t joint_actual_position[DPM_NUM_JOINTS]; //actual position of the drives
     int32_t joint_actual_velocity[DPM_NUM_JOINTS]; //actual velocity of the drives
     int32_t joint_actual_torque[DPM_NUM_JOINTS]; //actual torque applied by the drives
     uint64_t digital; //state of upto 64 digital ins
     float analog[DPM_NUM_ANALOGS]; //state of float ins
-    int32_t  integer32[DPM_NUM_INT32S]; //status of signed integers ins
+    int32_t integer32[DPM_NUM_INT32S]; //status of signed integers ins
     uint8_t reserved[4];
     uint32_t unsigned32[DPM_NUM_UINT32S]; //status of unsigned integer ins
 }__attribute__((packed)) dpm_in_t;
@@ -48,14 +48,14 @@ typedef struct {
     uint32_t machine_word; // commanded (CiA-402) state of machine
     uint32_t hlc_control_word; // we dont what this is for
     uint32_t gbc_control_word; // move gbc fault and move not op en
-    uint32_t  heartbeat; // a periodically increasing heartbeat used to detect we are connected
-    uint16_t  joint_controlword[DPM_NUM_JOINTS]; // CiA-402 control word for the drives (not used if say GBEM is commanding teh drives)
+    uint32_t heartbeat; // a periodically increasing heartbeat used to detect we are connected
+    uint16_t joint_controlword[DPM_NUM_JOINTS]; // CiA-402 control word for the drives (not used if say GBEM is commanding teh drives)
     int32_t joint_set_position[DPM_NUM_JOINTS]; // set position for drives (usually only this is used not velocity and torque)
     int32_t joint_set_velocity[DPM_NUM_JOINTS]; // set velocity for drives
     int32_t joint_set_torque[DPM_NUM_JOINTS]; // set torque for drives
     uint64_t digital; //commanded state for digital outs
     float analog[DPM_NUM_ANALOGS]; // commanded values for float outs
-    int32_t  integer32[DPM_NUM_INT32S]; // commanded values for signed integer outs
+    int32_t integer32[DPM_NUM_INT32S]; // commanded values for signed integer outs
     uint8_t reserved[4];
     uint32_t unsigned32[DPM_NUM_UINT32S]; // commandsed values for unsigned integer outs
 }__attribute__((packed)) dpm_out_t;
@@ -68,5 +68,17 @@ extern uint8_t outB[SIZE_OF_GBC_PDO];
 
 extern dpm_in_t *dpm_in;
 extern dpm_out_t *dpm_out;
+
+
+//gbem->gbc
+typedef struct {
+    uint32_t drive_error_string[300][DPM_NUM_JOINTS];
+}__attribute__((packed)) dpm_offline_in_t;
+
+//gbc->gbem
+typedef struct {
+    uint32_t gbem_boot_mode;
+}__attribute__((packed)) dpm_offline_out_t;
+
 
 #endif
