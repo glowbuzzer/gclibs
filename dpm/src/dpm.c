42312a33cfe979e233107ed168d4488fd54a7c5f
David <david@glowbuzzer.com>
2023-09-04 14:48:45 +0100

    chore: Add virtual drive support and AW robot configs

--- dpm/src/dpm.c
+++ dpm/src/dpm.c
@@ -15,15 +15,10 @@
 #include "dpm.h"
 //#include "shared.h"
 
-//allows for double buffering but not used
-#ifdef CORE_CM7
-dpm_in_t *dpm_in = (dpm_in_t*)inA;
-dpm_out_t *dpm_out = (dpm_out_t*)outA;
-#endif
-#if defined(CORE_CM4) || defined(GB_APP_LINUX)
-dpm_in_t *dpm_in = (dpm_in_t*)inA;
-dpm_out_t *dpm_out = (dpm_out_t*)outA;
 
-#endif
+dpm_in_t *dpm_in = (dpm_in_t *) inA;
+dpm_out_t *dpm_out = (dpm_out_t *) outA;
+
+
 
 
