840bf1c831093df51c751960b8e90770f9a29690
David <david@glowbuzzer.com>
2023-10-25 08:09:40 +0100

    feat(#69): Add support for CST/CSV position monitoring

--- common-misc/inc/fault_cause.h
+++ common-misc/inc/fault_cause.h
@@ -17,14 +17,14 @@
 
 
 /* fault cause bit numbers in machine status word */
-#define FAULT_CAUSE_ESTOP_BIT_NUM                   	(0)
-#define FAULT_CAUSE_DRIVE_FAULT_BIT_NUM             	(1)
-#define FAULT_CAUSE_GBC_FAULT_REQUEST_BIT_NUM       	(2)
-#define FAULT_CAUSE_HEARTBEAT_LOST_BIT_NUM          	(3)
-#define FAULT_CAUSE_LIMIT_REACHED_BIT_NUM          	 	(4)
-#define FAULT_CAUSE_DRIVE_STATE_CHANGE_TIMEOUT_BIT_NUM	(5)
-#define FAULT_CAUSE_DRIVE_FOLLOW_ERROR_BIT_NUM         	(6)
-#define FAULT_CAUSE_DRIVE_NO_REMOTE_BIT_NUM         	(7)
+#define FAULT_CAUSE_ESTOP_BIT_NUM                    (0)
+#define FAULT_CAUSE_DRIVE_FAULT_BIT_NUM                (1)
+#define FAULT_CAUSE_GBC_FAULT_REQUEST_BIT_NUM        (2)
+#define FAULT_CAUSE_HEARTBEAT_LOST_BIT_NUM            (3)
+#define FAULT_CAUSE_LIMIT_REACHED_BIT_NUM                (4)
+#define FAULT_CAUSE_DRIVE_STATE_CHANGE_TIMEOUT_BIT_NUM    (5)
+#define FAULT_CAUSE_DRIVE_FOLLOW_ERROR_BIT_NUM            (6)
+#define FAULT_CAUSE_DRIVE_NO_REMOTE_BIT_NUM            (7)
 #define FAULT_CAUSE_ECAT_BIT_NUM                        (8)
 #define FAULT_CAUSE_DRIVE_ALARM_BIT_NUM                 (9)
 #define FAULT_CAUSE_GBC_INTERNAL_ERROR_BIT_NUM          (10)
@@ -34,4 +34,5 @@
 #define FAULT_CAUSE_HOMING_ERROR_BIT_NUM                (14)
 #define FAULT_CAUSE_GBC_TO_PLC_CON_ERROR_BIT_NUM        (15)
 #define FAULT_CAUSE_MOVE_NOT_OP_EN_BIT_NUM              (16)
+#define FAULT_CAUSE_CST_CSV_POSITION_LIMIT_ERROR_BIT_NUM (17)
 #endif //GCLIB_FAULT_CAUSE_H
